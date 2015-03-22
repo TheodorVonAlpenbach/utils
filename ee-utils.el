e7310bd4e25c69478c41d0c90a0d3001197f506d
Mats Bergstrøm <mbe@contango.no>
2015-03-22 23:13:41 +0100

    Added read-from-string-safe

--- ee-utils.el
+++ ee-utils.el
@@ -1,5 +1,12 @@
 (defvar *buffer-column-base* 1)
 
+(defun read-from-string-safe (string &optional start end)
+  "Same as `read-from-string' but returns (nil . nil) if reaching the end of STRING."
+  (condition-case nil
+      (read-from-string string start end)
+    (error (list nil))))
+;;(read-from-string-safe "")
+
 (cl-defun line-column->point (line column &optional (buffer (current-buffer)))
   (with-buffer buffer
     (goto-char (point-min))
