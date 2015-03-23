337ebafcb04de00074fe5d0ccc6d731a013003be
MBe <Mats Bergstrm>
2015-03-23 13:13:53 +0100

    New structure

--- lisp/graphics/mb-gnuplot.lisp
+++ lisp/graphics/mb-gnuplot.lisp
@@ -13,6 +13,37 @@ file. Returns the pathname of the stream together with the value of last form in
      (values (pathname ,stream)
 	     (prog1 (progn ,@body) (close ,stream)))))
 
+(defvar *gp-unique-id* 0)
+(defun gp-unique-id (&optional reset)
+  (when reset
+    (warn "Resetting unique ID")
+    (setf *gp-unique-id* 0))
+  (prog1 (format nil "~4,'0d" *gp-unique-id*)
+    (incf *gp-unique-id*)))
+;;(gp-unique-id)
+
+(defun gp-unique-name (&key (prefix "gp") type reset)
+  (format nil "~a~a~@[.~a~]" prefix (gp-unique-id reset) type))
+;;(gp-unique-name :prefix "gp" :type "pdf")
+
+(defun gp-directory (directory &optional (tmp-dir "/tmp/"))
+  (if directory
+    (if (char= (last-elt directory) #\/)
+      directory
+      (concatenate 'string directory "/"))
+    tmp-dir))
+;;(mapcar #'gp-directory '(nil "qwe" "qwe/"))
+
+(defun gp-filename (name type)
+  (if name
+    (format nil "~a~@[.~a~]" name type)
+    (gp-unique-name :type type)))
+;;(gp-filename "mats" "pdf")
+
+(defun gp-path (directory name type)
+  (concatenate 'string (gp-directory directory) (gp-filename name type)))
+;;(gp-path "dir" "mats" "pdf")
+
 (defun write-function (stream function &key x-values (resolution 100))
   "Returns the filename containing RESOLUTION samples of FUNCTION in the interval X-VALUES.
 TODO: Handle n-ary functions and generalize X-VALUES to N dimensions."
@@ -50,72 +81,74 @@ TODO:
       (apply #'write-data-1 stream target plist))))
 ;;(write-data `(:d ,#'sqrt) :resolution 10)
 
-(defun line-1 (data &key (with :lines) title x-values)
+(defun line-1 (out data &key (with :lines) title)
   "Converts GP-LINES and PLIST to gnuplot string and writes it to
 STREAM. It returnes the path to the DATAFILE."
-  (awhen (write-data data :x-values x-values)
+  (awhen (write-data data)
     (let ((title (case title
 		   (:default nil)
 		   ((:notitle nil) "notitle")
 		   (t (format nil "title \"~a\"" title)))))
-      (values (format nil "'~a' with lines~@[ ~a~]" (namestring it) title)
-	      it))))
-;;(line-1 `(:d ,#'sqrt :x-values (0 2)) :title "qwe" :x-values '(0.3 0.7))
+      (format out "'~a' with lines~@[ ~a~]" (namestring it) title))))
+;;(line-1 t `(:d ,#'sqrt :x-values (0 2)) :title "qwe")
 
-(defun line (expression)
+(defun line (out expression)
   "Converts GP-LINES and title to gnuplot string and writes it to
 STREAM.
 Here graph and plot is the same"
   (if (and (consp expression) (eql (first expression) :l))
-    (apply #'line-1 (rest expression))
+    (apply #'line-1 out (rest expression))
     ;; else expression is just the data expression
-    (line-1 expression)))
+    (line-1 out expression)))
 ;;(line `(:l (:d ,#'sqrt :x-values (0 2)) :title "qwe" :x-values (0.3 0.7)))
-;;(line `(:d ,#'sqrt :x-values (0 2)))
+;;(line t `(:d ,#'sqrt :x-values (0 2)))
+;;(line t #'sqrt)
 
-(defun graph (expression)
+(defun graph-p (expression)
+  (and (consp expression)
+       (member (first expression) '(:g :p))))
+
+;;concat
+(defun graph (out expression &key x-range y-range)
   "Converts GP-LINES and title to gnuplot string and writes it to
-STREAM.
-Here graph and plot is the same"
-  (concat
-   (if (and (consp expression) (member (first expression) '(:g :p)))
-     ;; extract and process properties 
-     (loop for x on (rest expression)
-	   until (keywordp (first x))
-	   collect (line (first x))
-	   ;; finally collect (process-properties x)
-	  )
-     ;; else expression is a simple line expression
-     (list (line expression)))
-   :pre "plot " :in ", "))
-;;(graph `(:g (:d ,#'sqrt :x-values (0 2)) (:d ,#'sqrt :x-values (0 2))))
-;;(graph `(:d ,#'sqrt :x-values (0 2)))
-;;(graph #'sqrt)
-;;(trace graph)
+STREAM. Here graph and plot is the same"
+  (when expression
+    (princ "plot " out)
+    (format-list out (if (graph-p expression)
+		       (rest expression)
+		       (listify expression))
+		 #'(lambda (out x) (line out x))
+		 :in ", ")))
+;;(graph t `(:g (:d ,#'sqrt :x-values (0 2)) (:d ,#'sqrt :x-values (0 2))))
+;;(graph t `(:d ,#'sqrt :x-values (0 2)))
+;;(graph t `(,#'sqrt ,#'sqrt))
+;;(untrace graph)
+(progn (princ "plot " t)(format t "1"))
 
-(defun graphs (expressions)
-  "Converts GP-LINES and title to gnuplot string and writes it to
-STREAM."
-  (concat expressions :in (newline) :key #'graph))
-;;(graphs `(,#'sqrt))
-;;(trace graphs)
-
-(defun script (expressions)
+(defun script (scriptpath expression terminal)
   "Returns a gnuplot script for plotting unary FUNCTION from A to B
 with N points."
-  (with-temporary-file (s)
-    (format s "set terminal pdf~%")
-    (format s "set output '~a.pdf'~%" (namestring s))
-    (format s "~a" (graphs expressions))))
-;;(script `(,#'sqrt))
+  (with-open-file (out scriptpath :direction :output :if-exists :supersede)
+    (let* ((type (terminal->type terminal))
+	   (target (namestring (make-pathname :type type :defaults out))))
+      (format out "set terminal ~a~%" type)
+      (format out "set output '~a'~%" target)
+      (graph out expression)
+      target)))
+;;(script nil `(:g ,#'sqrt ,#'sqrt) :pdf)
 ;;(trace script)
 
-(defun plot (&rest expressions)
+(defun terminal->type (terminal)
+  (string-downcase (symbol-name (first (listify terminal)))))
+;;(terminal->type :pdf)
+
+(defun plot (expression &key directory name (terminal :pdf))
   "Returns a gnuplot script for plotting unary FUNCTION from A to B
 with N points."
-  (destructuring-bind (scriptpath datapath)
-      (maptree #'namestring (multiple-value-list (script expressions)))
-    (ext:execute "/usr/bin/gnuplot" (namestring scriptpath))
-    (list :script scriptpath :data datapath :pdf (format nil "~a.pdf" scriptpath))))
+  (let* ((scriptpath (gp-path directory name "gp"))
+	(target (script scriptpath expression terminal)))
+    (ext:execute "/usr/bin/gnuplot" scriptpath)
+    (list :script scriptpath :target target)))
 ;;(gp:plot `(:p ,#'sqrt ,#'sq (:l (:d ,#'(lambda (x) (sq (sin x))) :x-values (0.01 1)) :title "Geir")))
 ;;/ssh:ssh:/
+;;(ext:execute "/usr/bin/gnuplot" "/tmp/gp0017.gp")
