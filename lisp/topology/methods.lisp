d4914257e06acef2edd40db55c932b470ae584cf
MBe <Mats Bergstrm>
2015-11-06 17:40:23 +0100

    Removed LENGTH

--- lisp/topology/methods.lisp
+++ lisp/topology/methods.lisp
@@ -1,7 +1,4 @@
 (in-package :topology)
 
-(defgeneric length (geometry)
-  (:documentation "Returns the area of 1D GEOMETRY."))
-
 (defgeneric area (geometry)
   (:documentation "Returns the area of 2D GEOMETRY."))
