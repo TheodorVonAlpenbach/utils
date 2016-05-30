409f7383072331320a7cc1cca100b7b5c6de21df
Mats Bergstrom <mbe@azure.lightstructures.no>
2016-05-30 16:09:11 +0200

    Removed obsolete dictionaries

--- elisp/dictionary/dic-map.el
+++ elisp/dictionary/dic-map.el
@@ -1,7 +1,6 @@
 (defvar *dic-map* (make-sparse-keymap "Dictionary"))
 
 (require 'dictionary "dict")
-(require 'store-norske)
 
 (define-key global-map "\C-cd" *dic-map*)
 (define-key *dic-map* "a" (dic-lookup-word "acronym")) ;acronym finder
@@ -9,10 +8,8 @@
 (define-key *dic-map* "B" (dic-lookup-word "ny"))	;nynorskordboka
 (define-key *dic-map* "d" (dic-lookup-word "de-eng2")) ;travlangs
 (define-key *dic-map* "D" (dic-lookup-word "de-eng")) ;dwds
-;;(define-key *dic-map* "d" (dic-lookup-word "de-eng")) ;ectaco
 (define-key *dic-map* "e" (dic-lookup-word "es-eng")) ;collins
 (define-key *dic-map* "f" (dic-lookup-word "fr-eng"))
-;(define-key *dic-map* "f" (dic-lookup-word "fr-eng2")) ;ectaco
 (define-key *dic-map* "g" (dic-lookup-word "lsj"))	;liddel-scott-jones
 (define-key *dic-map* "i" (dic-lookup-word "it-eng2")) ;ectaco
 (define-key *dic-map* "l" (dic-lookup-word "ls"))	;lewis & short
@@ -20,9 +17,6 @@
 (define-key *dic-map* "n" (dic-lookup-word "ne-eng"))	;ectaco
 (define-key *dic-map* "\C-n" (dic-lookup-word "ne-no"))	;travels
 (define-key *dic-map* "p" 'dic-lookup-proxy) ;travels
-(define-key *dic-map* "s" 'sn-lookup-word) ;store norske
-;;(define-key *dic-map* "s" (dic-lookup-word "se-eng")) ;ectaco
-;;(define-key *dic-map* "S" (dic-lookup-word "saob")) ;ectaco
 (define-key *dic-map* "w" (dic-lookup-word "mw"))	;merriam webster
 
 (define-key *dic-map* "\C-b" #'dic-browse-last-entry)
@@ -38,10 +32,6 @@
 	      "Forward to mw: "
 	      (mapcar #'list (dic-parse-output "it-eng")))))
       (dic-lookup word "mw")))
-
-;; interactive maps
-(require 'intermap)
-(define-key global-map "\C-cm" #'im-show-place)
 
 ;; interactive phone number 
 (require 'phone)
