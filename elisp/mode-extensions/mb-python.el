8dd3a97d556356df6fb0e0bcfeb95a50e7aae5b4
Mats Bergstrom <mbe@azure.lightstructures.no>
2016-08-24 15:35:41 +0200

    Added mb-python-last-sexp-region

--- elisp/mode-extensions/mb-python.el
+++ elisp/mode-extensions/mb-python.el
@@ -2,6 +2,12 @@
   "My local modifications of PYTHON-MODE"
   ;; Disable intraspace for INSERT-PARENTHESES
   (setq-local parens-require-spaces nil))
+   
+(defun mb-python-last-sexp-region ()
+  (save-excursion
+    (nreverse
+     (list (point)
+	   (1+ (re-search-backward "[[:space:]][^[:space:]]+"))))))
 
 (add-hook 'python-mode-hook #'set-mb-python-locals)
 
