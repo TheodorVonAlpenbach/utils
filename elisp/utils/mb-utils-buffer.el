f547b001ea527e3a336754d28290126d820a23bc
Mats Bergstrom <mbe@azure.lightstructures.no>
2016-11-03 15:07:03 +0100

    Added toggle-window-split

--- elisp/utils/mb-utils-buffer.el
+++ elisp/utils/mb-utils-buffer.el
@@ -390,5 +390,35 @@ If region is not active this function is equivalent with
 
 (defalias 'reverse-lines #'reverse-region)
 
+(defun toggle-window-split ()
+  "Toggle between horizontal and vertical split of two windows.
+Copied from http://stackoverflow.com/questions/14881020/\
+emacs-shortcut-to-switch-from-a-horizontal-split-to-a-vertical-split-in-one-move"
+  (interactive)
+  (if (= (count-windows) 2)
+      (let* ((this-win-buffer (window-buffer))
+         (next-win-buffer (window-buffer (next-window)))
+         (this-win-edges (window-edges (selected-window)))
+         (next-win-edges (window-edges (next-window)))
+         (this-win-2nd (not (and (<= (car this-win-edges)
+                     (car next-win-edges))
+                     (<= (cadr this-win-edges)
+                     (cadr next-win-edges)))))
+         (splitter
+          (if (= (car this-win-edges)
+             (car (window-edges (next-window))))
+          'split-window-horizontally
+        'split-window-vertically)))
+    (delete-other-windows)
+    (let ((first-win (selected-window)))
+      (funcall splitter)
+      (if this-win-2nd (other-window 1))
+      (set-window-buffer (selected-window) this-win-buffer)
+      (set-window-buffer (next-window) next-win-buffer)
+      (select-window first-win)
+      (if this-win-2nd (other-window 1))))))
+
+(global-set-key (kbd "C-x |") 'toggle-window-split)
+
 (provide 'mb-utils-buffer)
 
