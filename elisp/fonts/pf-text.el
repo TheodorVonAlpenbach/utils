198a7c3853d8b3b08621b9aa5e60bed0ee436256
TheodorVonAlpenbach <mbe@lightstructures.no>
2016-12-23 17:16:37 +0100

    New helper for bcircle

--- elisp/fonts/pf-text.el
+++ elisp/fonts/pf-text.el
@@ -58,10 +58,27 @@
 	    (cserif z3 (rdir) rleg))
 	   (pft-path z4 z5 z6)))))
 
-(defun bcircle-optimal-d (n)
-  "Returns optimal distance to control points given that
-The end points and min point lies on the unit circle."
-  (* (/ 4 3.0) (tan (/ pi (* 2 n)))))
-;;(bcircle-optimal-d 4)
-;;(- (sqrt 2) 1)
-;;(tan (/ pi 8))
+(defun bcircle (p1 p2 center)
+  "Return a bezier representation of a circular arch going from
+point P1 to point P2 on a circle centered at point CENTER. The
+bezier curve is formatted as the list (P1 C1 C2 P2), where C1 and
+C2 are the two control points.
+See http://stackoverflow.com/questions/734076/how-to-best-approximate-a-geometrical-arc-with-a-bezier-curve and the answer by MaxArt"
+  (let* ((u (vec center p1))
+	 (v (vec center p2))
+	 (u+v (vec-add u v))
+	 (k (bcircle-optimal-k (vec-angle u v)))
+	 (pe (vec-scalar-mult u+v (/ 2 (vec-sqr u+v)))))
+    (list p1
+	  (vec-add (vec-scalar-mult p1 (- 1 k))
+		   (vec-scalar-mult pe k))
+	  (vec-add (vec-scalar-mult p2 (- 1 k))
+		   (vec-scalar-mult pe k))
+	  p2)))
+;;(bcircle '(1 0) '(0 1) '(0 0))
+
+(cl-defun bcircle-optimal-k (angle)
+  "Return optimal bezier k for a circular arch of length ANGLE radians.
+For use of k, see `bcircle'."
+  (* (/ 4.0 (* 3 (+ (sec (/ angle 2)) 1)))))
+;;(bcircle-optimal-k (/ pi 2))
