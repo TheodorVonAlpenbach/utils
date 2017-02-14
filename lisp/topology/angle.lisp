2bc03b7b04fb1c8199480539793b9b8164a23da3
Mats Bergstrom <mbe@azure.lightstructures.no>
2017-02-14 16:34:00 +0100

    Added angle-between-segments. Corrected angle

--- lisp/topology/angle.lisp
+++ lisp/topology/angle.lisp
@@ -21,15 +21,20 @@ NUMERICS-UTILS::DETERMINANT."
 (defmethod cross-product ((x segment) (y (eql nil))) (second (coordinates (direction x))))
 
 ;;; ANGLE (no specialization so far)
-(defmethod abs-angle (x y)
+(defmethod abs-angle ((x point) (y point))
   "Absolute angle in radians between geometries X and Y.
 Absolute here means that the result is always in [0 PI] and that
 \(abs-angle X Y) == (abs-angle Y X)."
-  (acos (/ (inner-product x y)
-	   (norm (centre x)) (norm (centre y)))))
+  (acos (/ (inner-product x y) (norm x) (norm y))))
+
+(defmethod abs-angle ((x point) (y nil))
+  (acos (/ (inner-product x nil) (norm x))))
+
+(defmethod abs-angle (x y)
+  (abs-angle (centre x) (centre y)))
 
 (defmethod abs-angle (x (y (eql nil)))
-  (acos (/ (inner-product x y) (norm (centre x)))))
+    (acos (/ (inner-product x nil) (norm (centre x)))))
 
 (defmethod angle (x y)
   (if (minusp (cross-product x y))
@@ -38,6 +43,14 @@ Absolute here means that the result is always in [0 PI] and that
 ;;(angle '(1 0) '(-1 -1))
 ;;(angle '(-1 0) nil)
 ;;(angle (make-segment '(0 0) '(-1 0)) nil)
+;;(abs-angle (make-segment '(0 0) '(-1 0)) nil)
+;;(inner-product (make-segment '(0 0) '(-1 0)) nil)
+;;(norm (centre (make-segment '(0 0) '(-1 0))))
+;;(norm (centre nil))
+
+(defun angle-between-segments (s1 s2)
+  (angle (direction s1) (direction s2)))
+;;(angle-between-segments (ms 0 0 1 0) (ms 0 0 0 1))
 
 (defmethod orientation (x) (angle x nil))
 (defmethod orientation ((x point)) (orientation (coordinates x)))
