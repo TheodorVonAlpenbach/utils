748adaa48ec2c186d56f51e4e6719ecedffdae5f
Mats Bergstrom <mbe@azure.lightstructures.no>
2017-02-14 16:36:24 +0100

    Added all-segments-closer-between-p, line-intersect-p

--- lisp/topology/convex-hull.lisp
+++ lisp/topology/convex-hull.lisp
@@ -3,15 +3,6 @@
 ;;;; The concept of convexity is always in the anti-clockwise
 ;;;; direction in this module.
 
-;;shortcuts (only for testing)
-(defun ms (x1 y1 x2 y2) (make-segment `(,x1 ,y1) `(,x2 ,y2)))
-(defun mss (&rest xs-and-ys)
-  (loop for (s e) in (pairs (cut xs-and-ys))
-	collect (make-segment s e)))
-(defun mpl (&rest xs-and-ys) (make-path (cut xs-and-ys)))
-(defun mpg (&rest xs-and-ys) (make-polygon (cut xs-and-ys)))
-;;(mpg 0 0  1 0  1 1)
-
 (defmethod join-convex ((x point) (convex-segments cons))
   (loop for (cs . css) on convex-segments
 	for s = (make-segment x (start cs))
@@ -57,11 +48,56 @@
 
 (defmethod merge-convex-paths ((x convex-path) (y convex-path) &optional non-contiguous-p)
   ;;pseudo code
+  (declare (ignore non-contiguous-p))
   (merge-convex-paths
-   (subsequence x 0 -1)
+   (subseq* x 0 -1)
    (merge-convex-paths (last-elt x) (first y))
-   (subsequence y 1)))
+   (subseq* y 1)))
 
 ;;(unintern 'merge-convex-paths)
+(defun all-segments-closer-between-p (s1 s2)
+  "Return NIL iff a point is closer to S1 than S2 on a convex path." 
+  (let ((nu (normalize (g- (start s2) (end s1))))
+	(ns1 (normalize (direction s1)))
+	(ds2 (direction s2)))
+    (plusp (inner-product ds2 (g+ nu ns1)))))
+;;(all-segments-closer-between-p (ms -1 0 0 0) (ms 4 3 5 3))
 
+(defun all-segments-closer-between2 (s1 s2)
+  "Alternative implementation of all-segments-closer-between-p
+using intersection point." 
+  (awhen (line-intersect s1 s2)
+    (< (distance s1 it)
+       (distance s1 s2))))
 
+(defun all-segments-closer-between3 (s1 s2 ns1 ns2)
+  "Like all-segments-closer-between-p, but with preprocessed data"
+  (let ((nu (normalize (g- (start s2) (end s1)))))
+    (plusp (inner-product ns2 (g+ nu ns1)))))
+;;(let* ((s1 (ms -1 0 0 0)) (s2 (ms 4 3 5 3)) (ns1 (normalize (direction s1)))(ns2 (normalize (direction s2)))) (time (all-segments-closer-between3 s1 s2 ns1 ns2)))
+
+(defmethod line-intersect-p ((x cons) (y cons))
+  "Returns the intersection of lines X and Y.
+X are a pairs of points, (X1 X2) and (Y1 Y2), signifying the lines
+going through the points X1 and X2, and Y1 and Y2, respectively. The
+intersection points is calculating by solving for S in the following
+line equations:
+
+T*X2 + (1-T)*X1 = S*Y2 + (1-S)*Y1
+"
+  (let* ((dx (g- (second x) (first x)))
+	 (dy (g- (second y) (first y)))
+	 (dz (g- (first y) (second x)))
+	 (pdx (coordinates dx))
+	 (pdy (coordinates dy))
+	 (denominator (- (* (first pdy)
+			    (second pdx))
+			 (second pdy))))
+    (unless (zerop denominator) ;;otherwise no intersection --> NIL
+      (let ((s (/ (cross-product dx dz) denominator)))
+	(g+ (g* (second y) s)
+	    (g* (first y) (- 1 s)))))))
+;;(line-intersect-p (ms 0 0 1 0) (ms 4 3 5 3))
+
+(defmethod line-intersect-p ((x segment) (y segment))
+  (line-intersect-p (points x) (points y)))
