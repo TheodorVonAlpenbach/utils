c44ed4017f990c48641d66919263c3848fa21b59
Mats Bergstrom <mbe@azure.lightstructures.no>
2017-03-03 15:47:53 +0100

    Cleaned code

--- lisp/timeseries/turning-points.lisp
+++ lisp/timeseries/turning-points.lisp
@@ -9,75 +9,38 @@
   (abs (- (funcall key x) (funcall key y))))
 ;; (diff '(0 2) '(0 3/4) #'second)
 
-(defun turning-points (ts threshold &key (key #'identity) setkey debug)
+(defun setfsecond (x y) (setf (second x) y))
+(defun setfthird (x y) (setf (third x) y))
+
+(defun negate-if (pred number) (if pred (- number) number))
+
+(defun nudge (a b key setkey)
+  (destructuring-bind (a* b*) (mapcar key (list a b))
+    (let ((b-value (+ a* (negate-if (> a* b*) single-float-epsilon))))
+      (if setkey (funcall setkey b b-value) (setf b b-value))
+      b)))
+;;(nudge '(s 1) '(s 2) #'second #'setfsecond)
+;;(nudge 1 2 #'identity nil)
+
+
+(defun turning-points (ts threshold &key (key #'identity) setkey finalize-p debug)
   (let (res a b)
     (flet ((dp (x) (when debug (print x))))
-      (loop while ts do (dp `(:res ,res :ab (,a ,b) :ts ,ts)) do
+      (loop for stack on ts
+	    for c = (car stack) do (dp `(:res ,res :abc (,a ,b . ,c) :stack ,stack)) do
 	    (cond
-	      ((not a) (dp :cond0) (setf a (pop ts))) 
-	      ((not b) (dp :cond1) (setf b (pop ts)))
-	      ((not (turning-point-p a b (car ts) key)) (dp :cond2)
-	       (setf b (pop ts)))
+	      ((not a) (dp :cond0) (setf a c)) 
+	      ((not b) (dp :cond1) (setf b c))
+	      ((not (turning-point-p a b c key)) (dp :cond2)
+	       (setf b c))
 	      ((>= (diff a b key) threshold) (dp :cond3)
-	       (push a res) (setf a b b (pop ts)))
-	      ((turning-point-p a (car ts) b key) (dp :cond4)
-	       (setf a (pop ts))
-	       (if setkey
-		 (funcall setkey b (- (funcall key a) single-float-epsilon))
-		 (setf b (- (funcall key a) single-float-epsilon))))
-	      (t (dp :cond5) (pop ts))))
-      (dp `(:res ,res :ab (,a ,b) :ts ,ts))
-      (when (>= (diff a b key) threshold) (dp :cond6)
+	       (push a res) (setf a b  b c))
+	      ((turning-point-p a c b key) (dp :cond4)
+	       (setf a c  b (nudge a b key setkey)))
+	      (t (dp :cond5) c)))
+      (dp `(:res ,res :ab (,a ,b)))
+      (when (and finalize-p (>= (diff a b key) threshold)) (dp :cond6)
 	    (push a res))
-      (dp `(:res ,res :ab (,a ,b) :ts ,ts)))
+      (dp `(:res ,res :ab (,a ,b))))
     (nreverse res)))
 ;;(turning-points '(0 2 -2 -1 0 -1 2 0) 1 :debug t)
-
-;;; old implementation
-(defun same-direction-p (a b c key)
-	   
-  (destructuring-bind (a b c) (mapcar key (list a b c))
-    (or (<= a b c) (>= a b c))))
-
-(defun tp-prologue (ts key)
-  "Return first valid RES, A, B, TS constallation"
-  (let ((a (pop ts)))
-      (print ts)
-      (loop for (b c . r) on ts while c
-	    if (not (same-direction-p a b c key))
-	    return (list (list a) b c r)
-	    finally (return (list (list a) b nil ())))))
-;;(tp-prologue '(0 -1 0 2 0) #'identity)
-
-(defun tp-epilogue (ts a b key)
-  (nreverse (if b
-	      (if (same-direction-p (car ts) a b key)
-		(cons b ts)
-		(cons b (cons a ts)))
-	      (cons a ts))))
-
-(defun turning-points (ts threshold &key (key #'identity) debug)
-  (when (minusp threshold)
-    (error "THRESHOLD cannot be negative"))
-  (unless (listp ts)
-    (error "Timeseries must be a list"))
-  (if (< (length ts) 3)
-    ts
-    (loop with (res a b ts) = (tp-prologue ts key)
-	  while ts
-	  do (when debug (print (list :res res :ab (list a b) :ts ts)))
-	  do (if (same-direction-p a b (car ts) key)
-	       (setf b (pop ts))
-	       (if (>= (diff a b key) threshold)
-		 (setf res (cons a res)  a b  b (pop ts))
-		 (if (same-direction-p a (car ts) b key)
-		   (pop ts)
-		   ;;else pop ts to we find a TP
-		   (loop for r on ts
-			 for (c d . e) on ts
-			 while (same-direction-p a c d key)
-			 finally (setf a c  b d  ts e)))))
-	  finally (return (tp-epilogue res a b key)))))
-;;(turning-points '(0.3 -1.38 -0.73 0.8 0.84 2.24 0.43 1.55 2.32 2.87 1.14 1.75 1.25 1.07 2.46 1.61) 2)
-;;(turning-points '(0 -1 0 2 0 1 2 3 2 2.5) 3/2)
-;;(turning-points '((0 0) (0 1) (0 2) (0 3) (0 2) (0 1) (0 2) (0 1)) 3/2 :key #'second)
