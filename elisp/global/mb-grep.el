b1d945c8cd966eca03b8fb7c889f052b091cd2ff
Mats Bergstrom <mbe@azure.lightstructures.no>
2017-03-03 15:44:36 +0100

    Revived this module

--- elisp/global/mb-grep.el
+++ elisp/global/mb-grep.el
@@ -1,44 +1,42 @@
 (require 'c++-include)
 
-(defun* mb-grep (&optional (type (file-name-extension (buffer-name))))
+(cl-defun mb-grep-basic (&key (target (thing-at-point 'symbol))
+			      (directories
+			       (list (file-name-directory (buffer-file-name))))
+			      (types (list (file-name-extension (buffer-name)))))
+  "Convenient grep according to file type."
+  (when (stringp target)
+    (let ((globs (loop for x in (listify types) collect (concat "*." x))))
+      (compilation-start
+       (format "grep -nH -E '%s' %s"
+	 (substring-no-properties target)
+	 (concat* (combine (list directories globs) :key #'concat) :in " "))
+       'grep-mode))))
+;;(mb-grep-basic :target "mb-grep-basic" :types '("el"))
+
+(cl-defun mb-grep ()
   "Convenient grep according to file type."
   (interactive)
-  (let ((sexp (thing-at-point 'symbol)))
-    (setq grep-command 
-	  (cons (format "grep -n '%s' *.%s" sexp type)
-		(+ 9 (length sexp))))
-    (call-interactively 'grep)))
+  (mb-grep-basic))
 
-(defun* mb-grep1 (&optional (type (file-name-extension (buffer-name))))
+(cl-defun mb-grep1 ()
   "Convenient grep according to file type. Assumes a directory pyramid
 of height 1."
   (interactive)
-  (let ((sexp (thing-at-point 'symbol)))
-    (setq grep-command 
-	  (cons (format "grep -n '%s' ../*/*.%s" sexp type)
-		(+ 9 (length sexp))))
-    (call-interactively 'grep)))
+  (mb-grep-basic :directories "../*/*/"))
 
-(defun* mb-grep2 (&optional (type (file-name-extension (buffer-name))))
+(cl-defun mb-grep2 (&optional (type (file-name-extension (buffer-name))))
   "Convenient grep according to file type. Assumes a directory pyramid
 of height 0, ie. searches only in the same directory."
   (interactive)
-  (let ((sexp (thing-at-point 'symbol)))
-    (setq grep-command 
-	  (cons (format "grep -n '%s' ../../*/*/*.%s" sexp type)
-		(+ 9 (length sexp))))
-    (call-interactively 'grep)))
+  (mb-grep-basic :directories "../../*/*/*/"))
 
-(defun* mb-c++-grep ()
+(cl-defun mb-c++-grep ()
   "Convenient grep according to file type."
   (interactive)
-  (let ((sexp (thing-at-point 'symbol)))
-    (setq grep-command 
-	  (cons (format "grep -n '%s' *.cpp *.h" sexp)
-		(+ 9 (length sexp))))
-    (call-interactively 'grep)))
+  (mb-grep-basic :types "{h,cpp}"))
 
-(defun* mb-lisp-grep ()
+(cl-defun mb-lisp-grep ()
   "Convenient grep according to file type."
   (interactive)
   (let ((sexp (thing-at-point 'symbol)))
@@ -47,7 +45,7 @@ of height 0, ie. searches only in the same directory."
 		(+ 9 (length sexp))))
     (call-interactively 'grep)))
 
-(defun* clisp-grep ()
+(cl-defun clisp-grep ()
   "Convenient grep according to file type."
   (interactive)
   (let ((sexp (thing-at-point 'symbol)))
