7e2269735b7f6a42a73b713eaa96029e33f752fc
TheodorVonAlpenbach <mbe@lightstructures.no>
2017-03-20 21:45:21 +0100

    Moved cross-product to module cross-product

--- lisp/topology/angle.lisp
+++ lisp/topology/angle.lisp
@@ -1,28 +1,5 @@
 (in-package :topology)
 
-;;; CROSS-PRODUCT
-(defmethod cross-product ((x (eql nil)) (y cons))
-  "Special specialization, first argument is NIL representing the X-axis."
-  (second y))
-;;(cross-product nil '(1 1))
-
-(defmethod cross-product ((x cons) (y cons))
-  "Since we know this is 2D, we shouldn't invoke the heavy
-NUMERICS-UTILS::DETERMINANT."
-  (- (* (first x) (second y)) (* (second x) (first y))))
-;;(cross-product '(234 456) '(567 34))
-
-(defmethod cross-product ((x point) (y point))
-  (cross-product (coordinates x) (coordinates y)))
-(defmethod cross-product ((x segment) (y segment))
-  (cross-product (direction x) (direction y)))
-(defmethod cross-product ((x segment) (y point))
-  (cross-product (direction x) (g- y (start x))))
-
-(defmethod cross-product ((x cons) (y (eql nil))) (second x))
-(defmethod cross-product ((x point) (y (eql nil))) (second (coordinates x)))
-(defmethod cross-product ((x segment) (y (eql nil))) (second (coordinates (direction x))))
-
 ;;; ANGLE (no specialization so far)
 (defmethod abs-angle ((x point) (y point))
   "Absolute angle in radians between geometries X and Y.
