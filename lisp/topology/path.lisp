f41a78214010faa6ac293e8bd6af005934bf9329
TheodorVonAlpenbach <mbe@lightstructures.no>
2017-03-20 21:48:38 +0100

    path segments converted to a vector

--- lisp/topology/path.lisp
+++ lisp/topology/path.lisp
@@ -1,20 +1,22 @@
 (in-package :topology)
 
 (defclass path (geometry)
-  ((segments :initarg :segments :accessor segments :type list)))
+  ((segments :initarg :segments :accessor segments :type vector)))
+;;(make-instance 'path :segments (vector (ms 0 0  1 0)))
 
 (defmethod make-path (points)
   (make-instance 'path
-    :segments (loop for (start end) in (pairs points)
-		    collect (make-segment start end))))
+    :segments (coerce (loop for (start end) in (pairs points)
+			    collect (make-segment start end))
+		      'vector)))
 ;;(trace make-path)
 
-(defmethod head ((x path)) (first (segments x)))
+(defmethod head ((x path)) (elt (segments x) 0))
 (defmethod tail ((x path)) (last-elt (segments x)))
 (defmethod start ((x path)) (start (head x)))
 (defmethod end ((x path)) (end (tail x)))
 
 (defmethod print-object ((x path) stream)
   (print-unreadable-object (x stream :type t)
-    (princ (mapcar #'coordinates (points x)) stream)))
+    (princ (map 'list #'coordinates (points x)) stream)))
 ;;(make-path '((0 0) (1 0) (1 1) (0 1)))
