26bf9db4378b551cc3d46ac605a506706efa19b1
Mats Bergstrom <mbe@azure.lightstructures.no>
2017-03-20 16:07:16 +0100

    Added tests for project

--- elisp/utils/mb-sequences-tests.el
+++ elisp/utils/mb-sequences-tests.el
@@ -23,3 +23,23 @@
   "Test of nminimum-nokey"
   (should (equal (nminimum-nokey [5118 5002 5116] #'<) '(5002 1)))
   (should (equal (nminimum-nokey [5118 5002 5116] #'>) '(5118 0))))
+
+(ert-deftest test-project ()
+  "Test of PROJECT"
+  (should (equal '(b a c b) (project '(a b c) '(1 0 2 1))))
+  (should (equal '(a b c) (project '(a b c) t)))
+  (should (equal 'b (project '(a b c) 1)))
+  (should (equal 99 (project "abc" 2)))
+  (should (equal "c" (project "abc" '(2))))
+  (should (equal 2 (project '(0 1 2 3) #'(lambda (x) (elt x 2)))))
+  (should (equal '(0 1 2 (3))
+		 (project '(0 1 2 3) (list #'first
+					   #'second
+					   #'(lambda (x) (elt x 2))
+					   #'last)))))
+
+(ert-deftest test-project-sequence ()
+  "Test of PROJECT-SEQUENCE"
+  (should (equal '(49 57) (project-sequence '("01" "09") 1)))
+  (should (equal '("1" "9") (project-sequence '("01" "09") (list 1))))
+  (should (equal '((2 3) (5 6)) (project-sequence '((1 2 3) (4 5 6)) '(1 2)))))
