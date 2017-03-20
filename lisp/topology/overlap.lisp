34860b9469e775161da58fe6a5917515568d91b2
TheodorVonAlpenbach <mbe@lightstructures.no>
2017-03-20 21:48:03 +0100

    Tidying

--- lisp/topology/overlap.lisp
+++ lisp/topology/overlap.lisp
@@ -1,15 +1,16 @@
 (in-package :topology)
 
+(defmethod overlap-p ((p point) (s segment))
+  (zerop (distance2 p s)))
+;;(overlap-p (ms 0 0 .9 0) (ms 0 0 .9 0))
+
 (defmethod overlap-p ((s1 segment) (s2 segment))
   (destructuring-bind (t1 t2) (line-intersection-coeffs s1 s2)
     (if t1
       (and (<= 0 t1 1) (<= 0 t2 1))
       ;; s1 and s2 are parallel
       (overlap-p (start s1) s2))))
-
-(defmethod overlap-p ((p point) (s segment))
-  (zerop (distance2 p s)))
-;;(overlap-p (ms 0 0 .9 0) (ms 0 0 .9 0))
+;;(overlap-p (ms 0 0 3 0) (ms 1 0 2 0))
 
 (defmethod line-intersection ((s1 segment) (s2 segment))
   (let* ((u (g- (start s2) (start s1)))
