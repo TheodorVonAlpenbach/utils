ebdda159888a25682ec376ed8c9c501c0eb3d7a3
TheodorVonAlpenbach <mbe@lightstructures.no>
2017-09-13 22:15:20 +0200

    Corrected errors

--- elisp/games/risk.el
+++ elisp/games/risk.el
@@ -1,39 +1,54 @@
-(defun roll-dice () (1+ (random 6)))
-(defun roll-dices (n) (sort (loop repeat n collect (roll-dice)) #'>))
+(defun roll-dice ()
+  (random 6))
+;; (roll-dice)
+
+(defun roll-dices-no-sort (n)
+  (loop repeat n collect (roll-dice)))
+;;(roll-dices-no-sort 3)
+
+(defun roll-dices (n)
+  (sort (loop repeat n collect (roll-dice)) #'>))
 ;;(roll-dices 3)
 
-(defun risk-roll-1 (blacks reds)
-  "Returns the result of a single risk roll a list of booleans.
+(defun risk-roll-loss (blacks reds)
+  "Returns the losses in a single roll as (BLACKS-LOST REDS-LOST).
 T means a black win, NIL means a red win."
-  (loop for b in (roll-dices blacks)
-	for r in (roll-dices reds)
-	collect (> b r)))
-;;(risk-roll-1 3 2)
+  (let* ((bd (roll-dices blacks))
+	 (rd (roll-dices reds))
+	 (bres (cl-mapcar #'> bd rd)))
+    (list (count nil bres) (count t bres) bd rd)))
+;;(risk-roll-loss 3 2)
 
-(defun risk-roll (blacks reds)
-  "Returns the result of a single risk roll a list of booleans.
-T means a black win, NIL means a red win."
-  (let ((bres (risk-roll-1 blacks reds)))
-    (list (decf blacks (count t bres))
-	  (decf reds (count nil bres)))))
-;;(risk-roll 3 4)
-
-(defun risk-simulate-2 (blacks reds)
-  (let ((bres (cl-sort (roll-dices blacks) #'>))
-	(rres (cl-sort (roll-dices reds) #'>)))
-    (list bres rres)))
-;;(risk-simulate-2 3 2)
-
-(defun risk-simulate-1 (blacks reds)
-  )
+(cl-defun risk-roll-loss-prediction (blacks reds &optional (n 100000))
+  "Simulate N single rolls"
+  (mapcar #'average
+    (transpose (loop repeat n collect (head 2 (risk-roll-loss blacks reds))))))
+;;(risk-roll-loss-prediction 3 2)
+;;(0.421116 0.578884)
+;;(risk-roll-loss-prediction 1 1)
+;;(/ 5.0 12)
+;;(- 1 (/ 49.0 144))
 
 (defun risk-simulate (blacks reds)
-  )
+  (loop while (and (plusp blacks)
+		   (plusp reds))
+	for loss = (risk-roll-loss (min blacks 3) (min reds 2))
+	for (blacks-lost reds-lost) = loss
+	collect loss into losses
+	do (decf blacks blacks-lost)
+	do (decf reds reds-lost)
+	finally return (list blacks reds losses)))
+;;(risk-simulate 10 10)
 
-(defconst *dice* (a-b 1 6))
-(cl-defun possible-dices (n)
-  (if (plusp n)
-    (loop for config in (possible-dices (1- n))
-	  append (loop for x in *dice* collect (cons x config)))
-    (list nil)))
-;;(length (possible-dices 5))
+(cl-defun risk-simulate-prediction (blacks reds &optional (n 10000))
+  "Simulate N single rolls"
+  (let* ((res (loop repeat n collect (head 2 (risk-simulate blacks reds))))
+	 (bwins (copy-if #'(lambda (x) (apply #'> x)) res)))
+    (list (/ (length bwins) (float n))
+	  (average bwins :key #'first))))
+;;(risk-simulate-prediction 7 7 100)
+
+(cl-defun risk-simulate-prediction-table (blacks reds &optional (n 1000))
+  (combine (list (1-n blacks) (1-n reds))
+	   :key #'(lambda (x y) (risk-simulate-prediction x y n))))
+;;(time (risk-simulate-prediction-table 20 20))
