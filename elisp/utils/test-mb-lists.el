07492dc2ee834743221a57ef14f20fc084e95a61
Mats Bergstrom <mbe@azure.lightstructures.no>
2017-10-31 15:21:43 +0100

    Add test-tree-leaves, test-tree-member

--- elisp/utils/test-mb-lists.el
+++ elisp/utils/test-mb-lists.el
@@ -17,3 +17,23 @@
   (should (equal (combine '(("a" "b") ("c") ("e" "f")) :key #'concat)
 		 '("ace" "acf" "bce" "bcf"))))
 
+(ert-deftest test-tree-leaves ()
+  "Test of `tree-leaves'"
+  (should (equal (tree-leaves '()) nil))
+  (should (equal (tree-leaves '(a)) '(a)))
+  (should (equal (tree-leaves '(a (b))) '(b)))
+  (should (equal (tree-leaves '("a" ("b"))) '("b")))
+  (should (equal (tree-leaves '(a (b) (c))) '(b c)))
+  (should (equal (tree-leaves '(a (c (d (f))) (b (e)))) '(f e))))
+
+(ert-deftest test-tree-member ()
+  "Test of `tree-member'"
+  (should (equal (tree-member 'a ()) nil))
+  (should (equal (tree-member 'a '(a)) '(a)))
+  (should (equal (tree-member 'a '(a (b))) '(a (b))))
+  (should (equal (tree-member 2 '("a" ("bb" ("ccc"))) :key #'length)
+		 '("bb" ("ccc"))))
+  (should (equal (tree-member "b" '("a" ("b" ("c"))) :test #'string=)
+		 '("b" ("c"))))
+  (should (equal (tree-member 'b '(a (b (c)) (b (d))) :from-end nil) '(b (c))))
+  (should (equal (tree-member 'b '(a (b (c)) (b (d))) :from-end t) '(b (d)))))
