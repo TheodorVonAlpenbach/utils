5b4b0cf2edf3fa77f49c333bb1f127710e7e24c1
Mats Bergstrom <mbe@azure.lightstructures.no>
2017-11-29 14:03:27 +0100

    Merge

--- elisp/utils/test-mb-utils-strings.el
+++ elisp/utils/test-mb-utils-strings.el
@@ -1,4 +1,5 @@
 (require 'ert)
+(require 'mb-utils-strings)
 
 (ert-deftest test-string-trim ()
   "Test of `string-trim'."
@@ -8,4 +9,13 @@
   "Test of `read-whole-string'."
   (should (equal (read-whole-string "a b (c d)") '(a b (c d)))))
 
+(ert-deftest test-andcat ()
+  "Test of `andcat'"
+  (should (equal (andcat '()) ""))
+  (should (equal (andcat '("Peter")) "Peter"))
+  (should (equal (andcat '("Peter" "Mary")) "Peter and Mary"))
+  (should (equal (andcat '("Peter" "Paul" "Mary")) "Peter, Paul, and Mary"))
+  (should (equal (andcat '("Peter" "Paul" "Mary") "; ") "Peter; Paul; and Mary"))
+  (should (equal (andcat '("Peter" "Paulus" "Maria") ", " " und " " und ")
+	   "Peter, Paulus und Maria")))
 
