61daf887e562418da157ed0310c15a121667e781
Mats Bergstrom <mbe@azure.lightstructures.no>
2018-01-24 16:08:15 +0100

    Add push

--- elisp/utils/test-mb-utils-div.el
+++ elisp/utils/test-mb-utils-div.el
@@ -8,3 +8,16 @@
   (should (equal (all-equal 1 1) t))
   (should (equal (all-equal 1 2) nil)))
 
+(ert-deftest test-push ()
+  "Test of `push-' macros"
+  (let ((list '(a)))
+    (should (equal (push-back 'b list) '(a b)))
+    (should (equal list '(a b)))
+    (should (equal (push-back-list '() list) '(a b)))
+    (should (equal list '(a b)))
+    (should (equal (push-back-list '(c d) list) '(a b c d)))
+    (should (equal list '(a b c d)))
+    (should (equal (push-list '() list) '(a b c d)))
+    (should (equal list '(a b c d)))
+    (should (equal (push-list '(pre fix) list) '(pre fix a b c d)))
+    (should (equal list '(pre fix a b c d)))))
