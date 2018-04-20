b4247a7fedfdc470ade2f302c473f3a7c500b640
Mats Bergstrom <mbe@azure.lightstructures.no>
2018-04-20 15:57:00 +0200

    Reorganized key-maps. Corrected texinfo-@fiy

--- elisp/mode-extensions/mb-texinfo.el
+++ elisp/mode-extensions/mb-texinfo.el
@@ -1,11 +1,66 @@
 ;;;; This module contains extensions the Emacs' texinfo mode.
 
-(define-skeleton texinfo-insert-@xref
-  "Insert a `@xref{...}' command in a Texinfo buffer.
-A numeric argument says how many words the braces should surround.
-The default is not to surround any existing words with the braces."
-  (bow)
-  "@ref{" _ "}")
+(cl-defun texinfo-@fiy (function-name &optional (argument ""))
+  "Prepend \"@\" to FUNCTION-NAME and append curly parentheses.
+If ARGUMENT is a string insert it in the pair of curly parentheses."
+  (format "@%s{%s}" function-name argument))
+;;(texinfo-@fiy "foo" "bar")
+
+(defun texinfo-insert-@ (code)
+  (if (use-region-p)
+    (region-replace (format "@%s{%%s}" code))
+    (if (symbol-at-point)
+     (destructuring-bind (beg . end) (bounds-of-thing-at-point 'symbol)
+       (insert (texinfo-@fiy code (delete-and-extract-region beg end))))
+     (insert (format "@%s{}" code))
+     (backward-char 1))))
+
+(defmacro texinfo-def-insert-@-fn (code)
+  "Define an interactive function with only form (texinfo-insert-@ code)."
+  `(defun ,(intern (concat "texinfo-insert-@" code)) ()
+     "Not documented."
+     (interactive)
+     (texinfo-insert-@ ,code)))
+;;(texinfo-def-insert-@-fn "xref")
+
+(cl-defmacro texinfo-define-inserts
+    (&optional (symbols '(var xref ref pxref code result)))
+  `(progn ,@(loop for code in symbols collect
+		  `(texinfo-def-insert-@-fn ,(sstring code)))))
+(texinfo-define-inserts)
+;; (macroexpand `(progn ,@(loop for code in '(var xref ref pxref)
+;; 		  collect `(texinfo-def-insert-@-fn ,(sstring code)))))
+
+(defun mb-texinfo-ref-map ()
+  (let ((map (make-sparse-keymap)))
+    (define-key map "x" 'texinfo-insert-@xref)
+    (define-key map "r" 'texinfo-insert-@ref)
+    (define-key map "p" 'texinfo-insert-@pxref)
+    (define-key map "u" 'texinfo-insert-@uref)
+    map))
+
+(defun mb-texinfo-insert-map ()
+  (let ((map (make-sparse-keymap)))
+    (define-key map "c" 'texinfo-insert-@code)
+    (define-key map "d" 'texinfo-insert-@dfn)
+    (define-key map "D" 'texinfo-start-menu-description)
+    (define-key map "e" 'texinfo-insert-@end)
+    (define-key map "E" 'texinfo-insert-@emph)
+    (define-key map "f" 'texinfo-insert-@file)
+    (define-key map "i" 'texinfo-insert-@item)
+    (define-key map "k" 'texinfo-insert-@kbd)
+    (define-key map "m" 'texinfo-insert-@email)
+    (define-key map "n" 'texinfo-insert-@node)
+    (define-key map "o" 'texinfo-insert-@noindent)
+    (define-key map "q" 'texinfo-insert-@quotation)
+    (define-key map "r" 'texinfo-insert-@result)
+    (define-key map "s" 'texinfo-insert-@samp)
+    (define-key map "S" 'texinfo-insert-@strong)
+    (define-key map "t" 'texinfo-insert-@table)
+    (define-key map "u" 'texinfo-insert-@uref)
+    (define-key map "v" 'texinfo-insert-@var)
+    (define-key map "x" 'texinfo-insert-@example)
+    map))
 
 (defun mb-texinfo-map ()
   (let ((map (make-sparse-keymap))
@@ -25,32 +80,8 @@ The default is not to surround any existing words with the braces."
     (define-key map "{" 'texinfo-insert-braces)
     (define-key map "o" 'texinfo-insert-block)
 
-    (define-key map "r" ref-map)
-    (define-key ref-map "x" 'texinfo-insert-@xref)
-    (define-key ref-map ";" 'texinfo-insert-@xref)
-    (define-key ref-map "r" 'texinfo-insert-@ref)
-    (define-key ref-map "p" 'texinfo-insert-@pxref)
-    (define-key ref-map "u" 'texinfo-insert-@uref)
-
-    (define-key map "i" insert-map)
-    (define-key insert-map "c" 'texinfo-insert-@code)
-    (define-key insert-map "d" 'texinfo-insert-@dfn)
-    (define-key insert-map "D" 'texinfo-start-menu-description)
-    (define-key insert-map "e" 'texinfo-insert-@end)
-    (define-key insert-map "E" 'texinfo-insert-@emph)
-    (define-key insert-map "f" 'texinfo-insert-@file)
-    (define-key insert-map "i" 'texinfo-insert-@item)
-    (define-key insert-map "k" 'texinfo-insert-@kbd)
-    (define-key insert-map "m" 'texinfo-insert-@email)
-    (define-key insert-map "n" 'texinfo-insert-@node)
-    (define-key insert-map "o" 'texinfo-insert-@noindent)
-    (define-key insert-map "q" 'texinfo-insert-@quotation)
-    (define-key insert-map "s" 'texinfo-insert-@samp)
-    (define-key insert-map "S" 'texinfo-insert-@strong)
-    (define-key insert-map "t" 'texinfo-insert-@table)
-    (define-key insert-map "u" 'texinfo-insert-@uref)
-    (define-key insert-map "v" 'texinfo-insert-@var)
-    (define-key insert-map "x" 'texinfo-insert-@example)
+    (define-key map "r" (mb-texinfo-ref-map))
+    (define-key map "i" (mb-texinfo-insert-map))
 
     (define-key map "u" update-map)
     (define-key update-map "m" 'texinfo-make-menu)
