97369135d47fc13568672cf9aca8c02993d40326
Mats Bergstrom <mbe@azure.lightstructures.no>
2018-04-20 15:50:05 +0200

    Moved mb-js-mode-hook-function elsewhere

--- elisp/mode-extensions/mb-div-mode.el
+++ elisp/mode-extensions/mb-div-mode.el
@@ -8,11 +8,4 @@
   (setf comment-start "//")
   (setf comment-end ""))
 
-(defun mb-js-mode-hook-function ()
-  (linum-mode)
-  ;;(setq tab-width 4)
-  ;; to setup tabs
-  (setq js-indent-level 4)
-  (setq tab-width 4)
-  (setq indent-tabs-mode t))
 
