bf9fb7df40fb9605c40767f1758561ce016f8d98
Mats Bergstrom <mbe@azure.lightstructures.no>
2018-04-30 13:37:53 +0200

    Add string-head and string-tail

--- elisp/utils/test-mb-utils-strings.el
+++ elisp/utils/test-mb-utils-strings.el
@@ -9,6 +9,22 @@
   "Test of `read-whole-string'."
   (should (equal (read-whole-string "a b (c d)") '(a b (c d)))))
 
+(ert-deftest test-string-head ()
+  "Test of `string-head'"
+  (should (equal (string-head "line1\nline2\nline3\nline4") "line1"))
+  (should (equal (string-head "line1\nline2\nline3\nline4" 1) "line1"))
+  (should (equal (string-head "line1\nline2\nline3\nline4" 2) "line1\nline2"))
+  (should (equal (string-head "line1\nline2\nline3\nline4" 0) "")))
+
+(ert-deftest test-string-tail ()
+  "Test of `string-tail'"
+  (should (equal (string-tail "line1\nline2\nline3\nline4") "line4"))
+  (should (equal (string-tail "line1\nline2\nline3\nline4" 1) "line4"))
+  (should (equal (string-tail "line1\nline2\nline3\nline4" 2) "line3\nline4"))
+  (should (equal (string-tail "line1\nline2\nline3\nline4" 0) "")))
+
+
+
 (ert-deftest test-andcat ()
   "Test of `andcat'"
   (should (equal (andcat '()) ""))
