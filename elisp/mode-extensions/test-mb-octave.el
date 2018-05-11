b92e9b83c30b54cc5e54a6ba47db0b576ebe5741
Mats Bergstrom <mbe@azure.lightstructures.no>
2018-05-11 18:07:16 +0200

    New test

--- elisp/mode-extensions/test-mb-octave.el
+++ elisp/mode-extensions/test-mb-octave.el
@@ -1,6 +1,10 @@
 (require 'ert)
 (require 'mb-octave)
 
+(ert-deftest test-octave-buffers ()
+"Test of `octave-buffers'"
+ (should (every #'octave-mode-p (octave-buffers))))
+
 (ert-deftest test-texinfo-@fiy ()
   "Test of `andcat'"
   (should (equal (texinfo-@fiy "foo") "@foo{}"))
