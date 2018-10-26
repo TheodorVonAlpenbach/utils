ef407dd654962cf34dcab137de3d0d908ee420d2
Mats Bergstrom <mbe@aqua.lightstructures.no>
2018-10-26 14:59:46 +0200

    Added informative error message if method fail

--- elisp/external/ods.el
+++ elisp/external/ods.el
@@ -1,5 +1,10 @@
 (cl-defun ods->cvs (filename)
-  (call-process* "unoconv" "--stdout" "-f" "csv" filename))
+  "Convert ods document at FILENAME to an CSV string.
+Note! The document cannot be opened in LibreOffice for this to work"
+  (let ((res (call-process* "unoconv" "--stdout" "-f" "csv" filename)))
+    (if (string-match "^Traceback (most recent call last)" res)
+      (error "Conversion failed. Is the document open in LibreOffice?")
+      res)))
 ;;(ods->cvs "/home/mbe/projects/veracity/13_001_Miros_HeidrunFSO/sensor-names-13001.ods")
 
 (cl-defun read-ods (filename &optional sheet)
