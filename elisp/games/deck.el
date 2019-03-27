9dd570083065f08febbe899c5ef2fe2ef855ff5c
TheodorVonAlpenbach <mbe@lightstructures.no>
2019-03-27 09:42:06 +0100

    New version

--- elisp/games/deck.el
+++ elisp/games/deck.el
@@ -1,26 +1,10 @@
 (defconst +deck-value+ (append (cons 'A (a-b 2 9)) '(T J Q K)))
 
 (defconst +deck-color+
-  (list (propertize "♠" 'font-lock-face '(:foreground "black"))
-	(propertize "♥" 'font-lock-face '(:foreground "red"))
-	(propertize "♦" 'font-lock-face '(:foreground "red"))
-	(propertize "♣" 'font-lock-face '(:foreground "black"))))
-
-(cl-defun deck-insert-card-1 (sv sc &optional (pos (point)))
-  (if sv
-    (insert (format "%S%s" sv sc))
-    (insert (format " %s" sc))))
-;;(deck-insert-card-1 nil "a")
-
-(cl-defun deck-insert-card (n &optional (pos (point)))
-  (destructuring-bind (v c) (cl-floor n 13)
-    (deck-insert-card-1 (nth c +deck-value+) (nth v +deck-color+))))
-
-(cl-defun deck-format-card-1 (sv sc &optional (pos (point)))
-  (if sv
-    (format "%S%s" sv sc)
-    (format " %s" sc)))
-;;(deck-insert-card-1 nil "a")
+  (list (propertize "♠" 'font-lock-face '(:foreground "black" :height 3.0))
+	(propertize "♥" 'font-lock-face '(:foreground "red" :height 3.0))
+	(propertize "♦" 'font-lock-face '(:foreground "red" :height 3.0))
+	(propertize "♣" 'font-lock-face '(:foreground "black" :height 3.0))))
 
 (cl-defun deck-format-card (n &optional (pos (point)))
   (if n
@@ -29,6 +13,17 @@
       (destructuring-bind (v c) (cl-floor n 13)
 	(format "%S%s" (nth c +deck-value+) (nth v +deck-color+))))
     "  "))
+
+(cl-defun deck-format-card (n &optional (pos (point)))
+  (if n
+    (if (minusp n)
+      (concat (propertize " " 'font-lock-face '(:height 3.0))
+	      (nth (- -1 n) +deck-color+))
+      (destructuring-bind (v c) (cl-floor n 13)
+	(concat (propertize (format "%S" (nth c +deck-value+))
+			    'font-lock-face '(:height 3.0))
+		(nth v +deck-color+))))
+    (propertize "  " 'font-lock-face '(:height 3.0))))
 ;;(mapcar #'deck-format-card '(0 -1 -2 -3 -4 nil))
 
 (defun deck-shuffle ()
@@ -49,5 +44,12 @@
 	(list v c)
 	(+ (* 13 c) v)))))
 ;;(deck-parse-card "ah" t)
+
+(defface solitaire
+       '((t :height 3.0))
+       "Basic face for solitaire buffers."
+       :group 'basic-faces)
+;;(face-attribute 'solitaire :height)
+
 
 (provide 'deck)
--- elisp/games/klondike.el
+++ elisp/games/klondike.el
@@ -6,9 +6,6 @@
 ;;;; HAND is a ring of variable length ("all the other cards")
 ;;;; See 
 
-(defvar *klondike-state* nil)
-;;(setf *klondike-state* (klondike-init))
-
 (defalias 'klondike-hidden #'first)
 (defalias 'klondike-visible #'second)
 (defalias 'klondike-out #'third)
@@ -16,30 +13,23 @@
 ;;(klondike-hidden *klondike-state*)
 
 (defun klondike-init ()
-  (let ((deck (deck-shuffle))
-	(hand (make-ring 24)))
-    (loop for x in deck do (ring-insert hand x))
-    (list (loop for i from 1 to 7 collect (pop-list deck i))
+  (let ((deck (deck-shuffle)))
+    (list (loop for i from 1 to 7 collect (pop-list deck (1- i)))
 	  (mapcar #'list (pop-list deck 7))
-	  '(0 0 0 0)
-	  hand)))
+	  (list -1 -2 -3 -4)
+	  deck)))
 ;;(klondike-init)
+
+(defvar *klondike-state* nil)
+;;(setf *klondike-state* (klondike-init))
 
 (defun klondike-format-card (card)
   (format " %s" (deck-format-card card)))
 ;;(klondike-format-card 0)
 
-(defun klondike-insert-out (out)
-  (loop for c in out
-	for i from 0
-	do (insert " ")
-	do (if (plusp c)
-	     (deck-insert-card c)
-	     (deck-insert-card-1 nil (nth i +deck-color+)))))
-
 (defun klondike-format-out (out)
-  (concat* out :in " " :key #'klondike-format-card))
-;;(klondike-format-out '(0 1 -1 3))
+  (concat* out :key #'klondike-format-card))
+;;(klondike-format-out (klondike-out *klondike-state*))
 
 (defun klondike-format-visible (visible)
   (concat* (transpose
@@ -51,10 +41,10 @@
 ;;(klondike-insert-visible '(nil (1) (2) (3) (4) (7 5) (16)))
 
 (defun klondike-format-hand (hand)
-  (if (ring-empty-p hand)
+  (if (null hand)
     "Empty"
-    (klondike-format-card (ring-ref hand 0))))
-;;(klondike-insert-hand (klondike-hand *klondike-state*))A♥
+    (klondike-format-card (car hand))))
+;;(klondike-format-hand (klondike-hand *klondike-state*))
 
 (cl-defun klondike-insert-state (&optional (state *klondike-state*))
   (insert (klondike-format-out (klondike-out state)))
@@ -65,24 +55,125 @@
   (newline 2))
 ;;(klondike-insert-state)
 
+(cl-defun klondike-rotate-hand (&optional (n 1) (state *klondike-state*))
+  (rotatef-list (klondike-hand state) n))
+;;(klondike-rotate-hand)
+
 (cl-defun klondike-move-hand (&optional column (state *klondike-state*))
-  (let ((hand (klondike-hand state)))
-    (when (plusp (ring-length hand))
-      (let ((card (ring-remove hand 0)))
-	(if column
-	  (push card (nth column (klondike-visible state)))
-	  (setf (nth (deck-color card) (klondike-out state)) card))))))
+  "Move top card from hand and to either visible or out.
+If COLUMN is nil it moves the card to the apropriate OUT deck.
+Else move card to visible's COLUMNth deck."
+  (awhen (pop (klondike-hand state))
+    (if column
+      (push it (nth column (klondike-visible state)))
+      (setf (nth (deck-color it) (klondike-out state)) it))))
 ;;(klondike-move-hand 0)
 
 (cl-defun klondike-move-visible (card &optional column (state *klondike-state*))
-  "CARD is a symbol or string: as, ah, ad, ac, 2s, 2h, ..., kc"
+  "Move CARD from hand and to either visible or out.
+If COLUMN is nil, it moves the card to the apropriate OUT deck.
+This action requires that no card is pushed on it. Else move card
+and all cards pushed on it to visible's COLUMNth deck."
   (let ((cards (loop with c = (deck-parse-card card)
-		     for col in-ref (klondike-visible state)
-		     for pos = (position c col)
-		     if pos return (pop-list col (1+ pos)))))
+		     for visible-column in-ref (klondike-visible state)
+		     for hidden-column in-ref (klondike-hidden state)
+		     for pos = (position c visible-column)
+		     if pos
+		     return (prog1
+				(pop-list visible-column (1+ pos))
+			      (awhen (pop hidden-column)
+				(push it visible-column))))))
 
     (if column
       (push-list cards (nth column (klondike-visible state)))
       (setf (nth (deck-color (car cards)) (klondike-out state))
 	    (car cards)))))
 ;;(klondike-move-visible "9d")
+
+;;; User interface
+(defconst +klondike-buffer+ "*Klondike*")
+
+(defun klondike-new ()
+  (setf *klondike-state* (klondike-init))
+  (klondike-refresh))
+
+(defun klondike-restart ()
+  (interactive)
+  (when (yes-or-no-p "Are you sure you want to quit this solitaire?")
+    (klondike-new)))
+
+(defun klondike-quit ()
+  (interactive)
+  (when (yes-or-no-p "Are you sure you want to quit this solitaire?")
+    (kill-buffer)))
+
+(defun klondike-refresh ()
+  (interactive)
+  (with-buffer +klondike-buffer+
+    (kill-region (point-min) (point-max))
+    (klondike-insert-state)))
+
+(defun klondike-hand-out ()
+  (interactive)
+  (klondike-move-hand)
+  (klondike-refresh))
+
+(defun klondike-hand-out-visible ()
+  (interactive)
+  (let ((col (read-from-minibuffer "Move hand to visible column: ")))
+    (klondike-move-hand (string-to-number col)))
+  (klondike-refresh))
+
+(defun klondike-hand-next-card ()
+  (interactive)
+  (klondike-rotate-hand 1)
+  (klondike-refresh))
+
+(defun klondike-hand-previous-card ()
+  (interactive)
+  (klondike-rotate-hand -1)
+  (klondike-refresh))
+
+(defun klondike-move-visible-to-column ()
+  (interactive)
+  (let ((scard (read-from-minibuffer "Move visible card: "))
+	(col (read-from-minibuffer "... to column: ")))
+    (klondike-move-visible scard (string-to-number col)))
+  (klondike-refresh))
+
+(defun klondike-move-visible-out ()
+  (interactive)
+  (let ((scard (read-from-minibuffer "Move out card: ")))
+    (klondike-move-visible scard))
+  (klondike-refresh))
+
+(define-derived-mode klondike-mode text-mode "Klondike"
+  "Klondike mode, same as text mode, except return submits the response.
+\\{klondike-mode-map}"
+  ;; (setf buffer-read-only t)
+  (let ((hand-map (make-sparse-keymap))
+	(visible-map (make-sparse-keymap)))
+    (define-key evil-normal-state-local-map "n" #'klondike-restart)
+    (define-key evil-normal-state-local-map "q" #'klondike-quit)
+    (define-key evil-normal-state-local-map "r" #'klondike-refresh)
+    ;; hand map
+    (define-key evil-normal-state-local-map "h" hand-map)
+    (define-key hand-map "o" #'klondike-hand-out)
+    (define-key hand-map "v" #'klondike-hand-out-visible)
+    (define-key hand-map "n" #'klondike-hand-next-card)
+    (define-key hand-map "p" #'klondike-hand-previous-card)
+    ;; visible map
+    (define-key evil-normal-state-local-map "v" visible-map)
+    (define-key visible-map "o" #'klondike-move-visible-out)
+    (define-key visible-map "v" #'klondike-move-visible-to-column)))
+
+(cl-defun klondike (&optional (unused 1))
+  (interactive)
+  (switch-to-buffer +klondike-buffer+)
+  (setq buffer-face-mode-face
+	'(:family "DejaVu Sans" :height 200 :width semi-condensed))
+  (klondike-mode)
+  (klondike-new))
+;;(klondike)
+
+(provide 'klondike)
