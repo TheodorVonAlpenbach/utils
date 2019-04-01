46cd334dc4f25c0b700ee53f3d28a0fd338714e4
Mats Bergstrom <mbe@aqua.lightstructures.no>
2019-04-01 13:34:42 +0200

    Add parent-directory

--- elisp/utils/test-mb-utils-file.el
+++ elisp/utils/test-mb-utils-file.el
@@ -1,6 +1,12 @@
 (require 'ert)
 (require 'mb-utils-file)
 
+(ert-deftest test-parent-directory ()
+  "Test of `parent-directory'"
+  (should (equal (parent-directory "/a/b/c") "/a/b/"))
+  (should (equal (parent-directory "/a/b/c/") "/a/b/"))
+  (should (equal (parent-directory (parent-directory "/a/b/c/")) "/a/")))
+
 (ert-deftest test-directory-truename ()
   "Test of `directory-truename'"
   (should (equal (directory-truename "a/b") (file-truename "a/b")))
