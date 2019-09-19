71b36cff76504ea0671ad2ce947a86d84a01e721
TheodorVonAlpenbach <mbe@lightstructures.no>
2019-09-19 21:15:54 +0200

    Correct geometric-series

--- elisp/utils/test-mb-math-functions.el
+++ elisp/utils/test-mb-math-functions.el
@@ -3,9 +3,9 @@
 
 (ert-deftest test-geometric-series ()
   "Test of `geometric-series'"
-  (should (equal (geometric-series 1 0.5) 2.0))
-  (should (equal (geometric-series 1 0.5 :end 2) 1.5))
-  (should (equal (geometric-series 1 0.5 :end 3) 1.75))
-  (should (equal (geometric-series 1 0.5 :start 1 :end 3) 0.75)))
+  (should (equal (geometric-series 0.5) 2.0))
+  (should (equal (geometric-series 0.5 :end 2) 1.5))
+  (should (equal (geometric-series 0.5 :end 3) 1.75))
+  (should (equal (geometric-series 0.5 :start 1 :end 3) 0.75)))
 
 (provide 'test-mb-math-functions)
