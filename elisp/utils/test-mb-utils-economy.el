9436e6b95419f2686ab9ba45f5857e9f20fb5be5
TheodorVonAlpenbach <mbe@lightstructures.no>
2019-09-19 21:16:05 +0200

    Improvements

--- elisp/utils/test-mb-utils-economy.el
+++ elisp/utils/test-mb-utils-economy.el
@@ -3,7 +3,10 @@
 
 (ert-deftest test-growth ()
   "Test of `growth'"
-  (should (equal (growth 2 2 0.75 1.0) 0.0)))
+  ;; (should (equal (growth 2 2 0.75 1.0) 0.0))
+  (should (equalp (growth 0 2 10 1) 10))
+  (should (equalp (growth 1 2 10 1) 22))
+  (should (equalp (growth 2 2 10 1) 46)))
 
 
 (provide 'test-mb-utils-economy)
