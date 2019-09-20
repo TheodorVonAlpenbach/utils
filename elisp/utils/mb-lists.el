86da321879e2fd0cc2ac6a26114719288f02d706
Mats Bergstrom <mbe@aqua.lightstructures.no>
2019-09-20 16:52:58 +0200

    Added/rewrote tree functionality

--- elisp/utils/mb-lists.el
+++ elisp/utils/mb-lists.el
@@ -643,6 +643,12 @@ interpreted as the parent of X."
 ;;(relations->tree '((a b) (a c) (c d) (b e)) :reverse-p t)
 ;;(relations->tree '((a b) (a c) (c d) (b e) (f g)))
 
+(cl-defun tree->relations (tree)
+  (when tree
+    (loop for sn in (cdr tree)
+	  append (cons (list (car tree) (car sn)) (tree->relations sn)))))
+;;(tree->relations (car (relations->tree '((a b) (a c) (c d) (b e)))))
+
 (cl-defun tree-member (x tree &key (test #'eql) key from-end)
   "Return first subtree in TREE having X has its top node"
   (if (funcall test x (if key (funcall key (car tree)) (car tree)))
@@ -650,40 +656,54 @@ interpreted as the parent of X."
     (loop for cn in (if from-end (reverse (cdr tree)) (cdr tree))
 	  if (tree-member x cn :test test :key key :from-end from-end)
 	  return it)))
-;;(tree-member 'b '(a (b (c)) (b (d))) :from-end t)
+;;(tree-member 'b '(a (b (c)) (b (d))) :from-end t :key nil)
 
-(cl-defun prune-tree-to-target (x tree &key (test #'eql) key from-end)
-  "Remove all nodes in FAMILY-TREE not having X as successor.
-Note that the keyword :FROM-END never used in this version."
-  (when from-end
-    (warning "prune-tree-to-target warning: :from-end was set. No effect."))
-  (when (tree-member x tree :test test :key key)
-    (cons (car tree)
-	  (loop for sn in (cdr tree)
-		if (prune-tree-to-target x sn :test test :key key)
-		collect it))))
-;;(prune-tree-to-target 'b '(a (b (c)) (d (b))))
+(cl-defun prune-tree-to-target (x tree &key (test #'eql) key from-end with-subtree)
+  "Remove all nodes in TREE not having X as successor.
+If :WITH-SUBTREE is not nil it returns all nodes that have X as
+either its successor or predecessor. If :FROM-END is nil it
+traverses TREE from left to right otherwise in the other
+direction."
+  (if (funcall test x (if key (funcall key (car tree)) (car tree)))
+    (if with-subtree tree (list (car tree)))
+    (loop for sn in (if from-end (reverse (cdr tree)) (cdr tree))
+	  if (prune-tree-to-target
+	      x sn :test test :key key :with-subtree with-subtree)
+	  collect (list (car tree) it))))
+;;(prune-tree-to-target 'b '(a (b (c)) (c) (d (b)) (f)) :with-subtree t)
 
-(cl-defun find-subtree (x trees
-			  &key reverse-p (test #'eql) key from-end tree-from-end)
+(cl-defun copy-subtree-1 (x trees copy-p filter test key from-end tree-from-end)
+  "Helper for `copy-subtree' and `find-subtree'."
+  (loop for ft in (if from-end (reverse trees) trees)
+	for res = (case filter
+		    (:down (tree-member
+			    x ft :test test :key key :from-end tree-from-end))
+		    (:up (prune-tree-to-target
+			  x ft :test test :key key
+			  :from-end tree-from-end :with-subtree nil))
+		    (:prune (prune-tree-to-target
+			     x ft :test test :key key
+			     :from-end tree-from-end :with-subtree t))
+		    (otherwise
+		     (error "Keyword :FILTER must be :UP, :DOWN, or :PRUNE.")))
+	if res if copy-p collect res else return res))
+;;(copy-subtree-1 'f '((a (b (f (d)))) (e (f (g (h))))) nil :up #'eql nil nil nil)
+
+(cl-defun find-subtree (x trees &key (filter :down) (test #'eql) key
+				  from-end tree-from-end)
   "Find the first subtree in TREES with X as top node.
 If REVERSE-P is not nil, return the minimal ancestor tree
 containing X."
-  (loop for ft in (if from-end (reverse trees) trees)
-	if (funcall (if reverse-p #'prune-tree-to-target #'tree-member)
-	     x ft :test test :key key :from-end tree-from-end)
-	return it))
+  (copy-subtree-1 x trees nil filter test key from-end tree-from-end))
+;;(find-subtree 'f '((a (b (f (d)))) (e (f (g (h))))) :filter :prune)
 
-(cl-defun copy-subtree (x trees
-			  &key reverse-p (test #'eql) key from-end tree-from-end)
-  "Find the first subtree in TREES with X as top node.
+(cl-defun copy-subtree (x trees &key (filter :down) (test #'eql) key
+				  from-end tree-from-end)
+  "Find all subtrees in TREES with X as top node.
 If REVERSE-P is not nil, return the minimal ancestor tree
 containing X."
-  (loop for ft in (if from-end (reverse trees) trees)
-	if (funcall (if reverse-p #'prune-tree-to-target #'tree-member)
-	     x ft :test test :key key :from-end tree-from-end)
-	collect it))
-;;(find-subtree 'e '((f (g)) (a (c (d)) (b (e)))) :reverse-p t)
+  (copy-subtree-1 x trees t filter test key from-end tree-from-end))
+;;(copy-subtree 'f '((a (b (f (d)))) (e (f (g (h))))) :filter :up)
 
 (defun tree-leaves (tree)
   (when tree
