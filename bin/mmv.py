6d1120e4ae508c7213c2ada3125048cee1846bc3
mats.bergstrom <mats.bergstrom@bouvet.no>
2020-09-03 08:47:18 +0200

    New

---
+++ bin/mmv.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import sys, shutil, os.path
+
+def printUsage():
+    print("Usage: mmv OLD NEW [FILE] ...") 
+    print("Rename all FILEs such that each occurrence of substring OLD is replaced by substring NEW.")
+
+def moveFile(old, new, path):
+    if os.path.isfile(path):
+        newpath = path.replace(old, new)
+        shutil.move(path, newpath)
+    else:
+        print("mmv: path '{0}' does not exist. Could not rename.".format(path)) 
+    
+nargs = len(sys.argv)
+
+if (nargs == 2) and ((sys.argv[1] == "--help") or (sys.argv[1] == "-h")):
+    printUsage()
+elif nargs < 3:
+    print("mmv: Too few arguments") 
+    printUsage()
+elif nargs == 3:
+    print("mmv: No files given. Nothing wil be done.") 
+    printUsage()
+else:
+    old = sys.argv[1]
+    new = sys.argv[2]
+    files = sys.argv[3:]
+    [moveFile(old, new, file) for file in files]
---
+++ elisp/mode-extensions/mb-java-mode.el
@@ -0,0 +1,13 @@
+(require 'cc-mode)
+;;java-mode
+
+(defun mb-java-mode-hook-function ()
+  (linum-mode)
+  (electric-pair-local-mode)
+  (setq java-indent-level 4)
+  (setq tab-width 4)
+  (setq indent-tabs-mode nil)
+  (setq-local parens-require-spaces nil))
+(add-hook 'java-mode-hook 'mb-java-mode-hook-function)
+
+(provide 'mb-js-mode)
