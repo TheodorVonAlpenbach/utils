5b1dcb84a928bdfb39f00920dae8a1998b893de5
mats.bergstrom <mats.bergstrom@bouvet.no>
2021-04-13 11:30:43 +0200

    New

---
+++ elisp/div/fizzbuzz.el
@@ -0,0 +1,15 @@
+(cl-defun fizz-buzz-sequence (&optional (n 20))
+  "Return the first N words in a correct Fizz buzz game sequence."
+  (loop for i from 1 to n collect
+	(if (zerop (mod i 3))
+	  (if (zerop (mod i 5))
+	    "Fizz Buzz" i)
+	  (if (zerop (mod i 5))
+	    "Buzz" i))))
+;;(fizz-buzz-sequence)
+
+(cl-defun fizz-buzz (&optional (n 20))
+  "Return a string with the first N words in a correct Fizz buzz
+game sequence."
+  (concat* (fizz-buzz-sequence n) :key #'sstring :in ", "))
+;;(fizz-buzz 35)
---
+++ elisp/div/scratch.el
@@ -0,0 +1,1 @@
+;;;; Better for elisp than *scratch* 
