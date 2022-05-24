0bb4058070d5d9afbbe481bd9fd7df0ff793530a
TheodorVonAlpenbach <mats@progfab.no>
2022-05-24 13:23:10 +0200

    Better help

--- bin/githubswitchuser.sh
+++ bin/githubswitchuser.sh
@@ -7,9 +7,16 @@ keyName=id_ed25519_cappelendamm
 
 # No argument: switch to MatsBergstromBouvet
 case $arg in
-    pf|progfab)
+    pf|progfab|id_rsa_pf)
 	keyName=id_rsa_pf
 	echo "Switching github user to TheodorVonAlpenbach (key name: $keyName)"
+	;;
+    -h|--help)
+	echo "Usage: githubswitchuser [id_ed25519_cappelendamm|id_rsa_pf]"
+	echo
+	echo "Without argument, the default id_ed25519_cappelendamm is applied."
+	echo "For arguments pf|progfab|id_rsa_pf, id_rsa_pf is applied."
+	echo "For all other arguments (except -h and --help) the default is applied."
 	;;
     *)
 	echo "Switching github user to CappelenDamm (key name: $keyName)"
