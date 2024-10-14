b0bdc230ba480da449dcce457b5f9dc087667dc9
Mats Bergstrøm <77495016+MatsBergstromBouvet@users.noreply.github.com>
2024-10-14 14:37:20 +0200

    Correct script. Add shortcuts

--- bin/githubswitchuser.sh
+++ bin/githubswitchuser.sh
@@ -2,25 +2,27 @@
 
 cd ~/.ssh
 
-arg=${1:-cappelendamm}
-keyName=id_ed25519_cappelendamm
+arg="${1:-cappelendamm}"
+algorithm="ed25519"
+prefix="id_${algorithm}"
+keyName="${prefix}_cappelendamm"
 
 # No argument: switch to MatsBergstromBouvet
 case $arg in
-    pf|progfab|id_rsa_pf)
-	keyName=id_rsa_pf
+    pf|progfab)
+	keyName="id_${algorithm}_pf"
 	echo "Switching github user to TheodorVonAlpenbach (key name: $keyName)"
 	;;
     -h|--help)
-	echo "Usage: githubswitchuser [id_ed25519_cappelendamm|id_rsa_pf]"
+	echo "Usage: githubswitchuser [id_ed25519_cappelendamm|id_ed25519_pf]"
 	echo
 	echo "Without argument, the default id_ed25519_cappelendamm is applied."
-	echo "For arguments pf|progfab|id_rsa_pf, id_rsa_pf is applied."
+	echo "For arguments pf|progfab|id_ed25519_pf, id_ed25519_pf is applied."
 	echo "For all other arguments (except -h and --help) the default is applied."
 	;;
     *)
 	echo "Switching github user to CappelenDamm (key name: $keyName)"
 esac
 
-cp masterkeys/$keyName ./id_rsa
-cp masterkeys/$keyName.pub ./id_rsa.pub
+cp "masterkeys/$keyName" "$prefix"
+cp "masterkeys/$keyName.pub" "$prefix.pub"
