6fcf45aec038f461e09218d00ff6b04e1478d89a
Mats Bergstrøm <77495016+MatsBergstromBouvet@users.noreply.github.com>
2024-10-14 13:58:45 +0200

    Correct script

--- bin/adaswitch.sh
+++ bin/adaswitch.sh
@@ -11,6 +11,12 @@ function printUsage {
     echo "  -v          Verbose mode"
 }
 
+if [ $# -eq 0 ]; then
+    echo "No arguments provided."
+    echo
+    printUsage
+fi
+
 while getopts "hv" arg; do
     case $arg in
 	h)
@@ -30,12 +36,15 @@ done
 shift $(( OPTIND-1 ))
 
 adaTag=`adatagtemplate $1`
-adaBranch=`git branch --list "$adaTag"*`
+adaBranchOrigin=`git branch -r --list "*$adaTag*"`
+adaBranch=`echo $adaBranchOrigin | cut -d '/' -f 2`
 
 if [ -n "$verbose" ]; then
     echo Verbose mode is on
     echo Script argument is \'$1\'
     echo adaTag is \'$adaTag\'
+    echo adaBranchOrigin is \'$adaBranchOrigin\'
+    echo adaBranch is \'$adaBranch\'
 fi
 
 if [ -n "$verbose" ]; then
